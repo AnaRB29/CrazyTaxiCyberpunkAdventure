a1fb91494abe9b52c0afdcfa5be19f7575fcf6cc
KaryVelle <kary.velle@gmail.com>
2023-10-12 19:04:31 -0600

    Avance castigo

--- CrazyTaxi/Assets/Movement.cs
+++ CrazyTaxi/Assets/Movement.cs
@@ -7,8 +7,9 @@ public class Movement : MonoBehaviour
     [SerializeField] public float rotDegrees = 0.5f;
     [SerializeField] public Rigidbody cabinRigidbody;
     [SerializeField] private Vector3 relativeFwd;
-    [SerializeField] private float speed;
+    [SerializeField] public float speed;
     [SerializeField] private float maxSpeed;
+    public bool playerHasControl;
     
     // Start is called before the first frame update
     void Start()
@@ -21,54 +22,63 @@ public class Movement : MonoBehaviour
     {
         
         relativeFwd = cabinRigidbody.transform.TransformDirection(Vector3.forward);
-        
-           if (Input.GetKey("w"))
-           cabinRigidbody.transform.eulerAngles = new Vector3(
-           cabinRigidbody.transform.eulerAngles.x - rotDegrees,
-            cabinRigidbody.transform.eulerAngles.y ,
-            cabinRigidbody.transform.eulerAngles.z) ;
+        if (playerHasControl)
+        {
+            if (Input.GetKey("w"))
+                cabinRigidbody.transform.eulerAngles = new Vector3(
+                    cabinRigidbody.transform.eulerAngles.x - rotDegrees,
+                    cabinRigidbody.transform.eulerAngles.y ,
+                    cabinRigidbody.transform.eulerAngles.z) ;
 
-           if ( Input.GetKey("s"))
-           {
-               cabinRigidbody.transform.eulerAngles = new Vector3(
-                   cabinRigidbody.transform.eulerAngles.x + rotDegrees,
-                   cabinRigidbody.transform.eulerAngles.y ,
-                   cabinRigidbody.transform.eulerAngles.z) ;
-           }
+            if ( Input.GetKey("s"))
+            {
+                cabinRigidbody.transform.eulerAngles = new Vector3(
+                    cabinRigidbody.transform.eulerAngles.x + rotDegrees,
+                    cabinRigidbody.transform.eulerAngles.y ,
+                    cabinRigidbody.transform.eulerAngles.z) ;
+            }
 
-           if (Input.GetKey("d"))
-           {
-               cabinRigidbody.transform.eulerAngles = new Vector3(
-                   cabinRigidbody.transform.eulerAngles.x ,
-                   cabinRigidbody.transform.eulerAngles.y + rotDegrees ,
-                   cabinRigidbody.transform.eulerAngles.z) ;     
+            if (Input.GetKey("d"))
+            {
+                cabinRigidbody.transform.eulerAngles = new Vector3(
+                    cabinRigidbody.transform.eulerAngles.x ,
+                    cabinRigidbody.transform.eulerAngles.y + rotDegrees ,
+                    cabinRigidbody.transform.eulerAngles.z) ;     
 
-           }
+            }
 
-           if (Input.GetKey("a"))
-           {
-               cabinRigidbody.transform.eulerAngles = new Vector3(
-                   cabinRigidbody.transform.eulerAngles.x ,
-                   cabinRigidbody.transform.eulerAngles.y - rotDegrees ,
-                   cabinRigidbody.transform.eulerAngles.z) ;
-           }
+            if (Input.GetKey("a"))
+            {
+                cabinRigidbody.transform.eulerAngles = new Vector3(
+                    cabinRigidbody.transform.eulerAngles.x ,
+                    cabinRigidbody.transform.eulerAngles.y - rotDegrees ,
+                    cabinRigidbody.transform.eulerAngles.z) ;
+            }
 
-           if (Input.GetKey("space"))
-           {
-               cabinRigidbody.velocity = relativeFwd * speed * maxSpeed;
-           }
+            if (Input.GetKey("space"))
+            {
+                cabinRigidbody.velocity = relativeFwd * speed * maxSpeed;
+            }
            
-           if (Input.GetKeyUp("space"))
-           {
-               cabinRigidbody.velocity = relativeFwd * speed * maxSpeed / 2;
-           }
+            if (Input.GetKeyUp("space"))
+            {
+                cabinRigidbody.velocity = relativeFwd * speed * maxSpeed / 2;
+            }
+            
+            if (Input.GetKey("q"))
+            {
+                cabinRigidbody.velocity = relativeFwd * speed * maxSpeed *-1;
+            }
+           
+            if (Input.GetKeyUp("q"))
+            {
+                cabinRigidbody.velocity = relativeFwd * speed * maxSpeed *-1 / 2;
+            }
 
-           if (Input.GetKey("e"))
-           {
-               cabinRigidbody.velocity = Vector3.zero;
-           }
-
-          
-                     
+            if (Input.GetKey("e"))
+            {
+                cabinRigidbody.velocity = Vector3.zero;
+            } 
+        }
     }
 }
